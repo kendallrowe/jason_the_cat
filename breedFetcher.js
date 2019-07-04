9d30eaf12fecf484686eea11e191ada767746d5f
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-04 16:31:52 +0000

    Seaparated index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,20 @@
 const request = require("request");
-const args = process.argv.slice(2);
 const searchURL = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const searchBreed = function(breed) {
-  request(searchURL + breed, (error, response, body) => {
-    const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
+  // request(searchURL + breedName, callback(error, body));
+  request(searchURL + breedName, (error, request, body) => {
     
     if (error) {
-      console.log('error:', error); // Print the error if one occurred
-    } else if (data[0] === undefined) {
-      console.log("Hm. We couldn't find the breed you requested. Try checking your search term.");
+      callback(error);
+    } else if (JSON.parse(body)[0] === undefined) {
+      callback("Hm. We couldn't find the breed you requested. Try checking your search term.");
     } else {
-      console.log(data[0].description); // Print the HTML for the Google homepage.
+      callback("", JSON.parse(body)[0].description);
     }
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log(typeof data);
+
   });
 };
 
-searchBreed(args[0]);
+
+module.exports = { fetchBreedDescription };
